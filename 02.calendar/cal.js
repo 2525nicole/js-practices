7cf4a6bf710ebeaa363ce3c4e78e20371255c97f
nicole2525 <nicole2525nomail@gmail.com>
2023-07-03 21:10:48 +0900

    JS版カレンダープログラムの作成

---
+++ 02.calendar/cal.js
@@ -0,0 +1,27 @@
+import minimist from "minimist";
+import dayjs from "dayjs";
+
+const argv = minimist(process.argv.slice(2));
+const base_date = dayjs()
+  .year(argv["y"] == null ? dayjs().format("YYYY") : argv["y"])
+  .month(argv["m"] == null ? dayjs().format("M") - 1 : Number(argv["m"]) - 1);
+const first_date = base_date.startOf("month");
+const last_date = base_date.endOf("month");
+const year_and_month = base_date.format("M月 YYYY");
+const day_of_week = "日 月 火 水 木 金 土";
+
+console.log(year_and_month.padStart(13));
+console.log(day_of_week);
+
+for (let date = first_date; date < last_date; date = date.add(1, "days")) {
+  let blank_number;
+  if (date == first_date) {
+    blank_number = Number(date.format("d")) * 3 + 2;
+  } else {
+    blank_number = 2;
+  }
+  process.stdout.write(String(date.date()).padStart(blank_number) + " ");
+  if (date.format("d") == 6) {
+    process.stdout.write("\n");
+  }
+}
