9e64ec3095a2758c20471b02d21a41a87601e217
nicole2525 <nicole2525nomail@gmail.com>
2023-07-09 11:55:36 +0900

    識別子を全てlowerCamelCaseに修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -2,25 +2,25 @@ import minimist from "minimist";
 import dayjs from "dayjs";
 
 const argv = minimist(process.argv.slice(2));
-const base_date = dayjs()
+const baseDate = dayjs()
   .year(argv["y"] == null ? dayjs().format("YYYY") : argv["y"])
   .month(argv["m"] == null ? dayjs().format("M") - 1 : Number(argv["m"]) - 1);
-const first_date = base_date.startOf("month");
-const last_date = base_date.endOf("month");
-const year_and_month = base_date.format("M月 YYYY");
-const day_of_week = "日 月 火 水 木 金 土";
+const firstDate = baseDate.startOf("month");
+const lastDate = baseDate.endOf("month");
+const yearAndMonth = baseDate.format("M月 YYYY");
+const dayOfWeek = "日 月 火 水 木 金 土";
 
-console.log(year_and_month.padStart(13));
-console.log(day_of_week);
+console.log(yearAndMonth.padStart(13));
+console.log(dayOfWeek);
 
-for (let date = first_date; date < last_date; date = date.add(1, "day")) {
-  let blank_number;
-  if (date === first_date) {
-    blank_number = Number(date.format("d")) * 3 + 2;
+for (let date = firstDate; date < lastDate; date = date.add(1, "day")) {
+  let blankNumber;
+  if (date === firstDate) {
+    blankNumber = Number(date.format("d")) * 3 + 2;
   } else {
-    blank_number = 2;
+    blankNumber = 2;
   }
-  process.stdout.write(String(date.date()).padStart(blank_number) + " ");
+  process.stdout.write(String(date.date()).padStart(blankNumber) + " ");
   if (date.format("d") === "6") {
     process.stdout.write("\n");
   }
