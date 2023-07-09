b460eb4374ae7acbaefb381716387746a467c224
nicole2525 <nicole2525nomail@gmail.com>
2023-07-09 21:07:36 +0900

    最終日出力後に改行を入れる処理を修正、最終日の判定式も修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -2,6 +2,8 @@ import minimist from "minimist";
 import dayjs from "dayjs";
 import ja from "dayjs/locale/ja.js";
 dayjs.locale(ja);
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore.js";
+dayjs.extend(isSameOrBefore);
 
 const argv = minimist(process.argv.slice(2));
 let baseDate = dayjs();
@@ -9,17 +11,21 @@ if (argv.y != null) baseDate = baseDate.set("year", argv.y);
 if (argv.m != null) baseDate = baseDate.set("month", argv.m - 1);
 const firstDate = baseDate.startOf("month");
 const lastDate = baseDate.endOf("month");
-const YearMonthDisplayPosition = 13;
+const yearMonthDisplayPosition = 13;
 const yearAndMonth = baseDate.format("MMM YYYY");
 const dayOfWeek = "日 月 火 水 木 金 土";
 let calcFirstdayDisplayPosition = (date) => {
   return date.get("d") * 3 + 2;
 };
 
-console.log(yearAndMonth.padStart(YearMonthDisplayPosition));
+console.log(yearAndMonth.padStart(yearMonthDisplayPosition));
 console.log(dayOfWeek);
 
-for (let date = firstDate; date < lastDate; date = date.add(1, "day")) {
+for (
+  let date = firstDate;
+  date.isSameOrBefore(lastDate);
+  date = date.add(1, "day")
+) {
   if (date === firstDate) {
     process.stdout.write(
       String(date.date()).padStart(calcFirstdayDisplayPosition(date)) + " "
@@ -28,8 +34,7 @@ for (let date = firstDate; date < lastDate; date = date.add(1, "day")) {
     const numberOfDigit = 2;
     process.stdout.write(String(date.date()).padStart(numberOfDigit) + " ");
   }
-  if (date.get("d") === 6) {
+  if (date.get("d") === 6 || date.isSame(lastDate, "day")) {
     process.stdout.write("\n");
   }
 }
-process.stdout.write("\n");
