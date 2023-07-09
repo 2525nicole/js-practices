5d7ae50d059ee74b2e55d7b34ded32f4cb3c1a8e
nicole2525 <nicole2525nomail@gmail.com>
2023-07-09 16:54:19 +0900

    年月表示位置の数字を変数に格納、表示開始位置を表す関数名も修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -9,19 +9,20 @@ if (argv.y != null) baseDate = baseDate.set("year", argv.y);
 if (argv.m != null) baseDate = baseDate.set("month", argv.m - 1);
 const firstDate = baseDate.startOf("month");
 const lastDate = baseDate.endOf("month");
+const YearMonthDisplayPosition = 13;
 const yearAndMonth = baseDate.format("MMM YYYY");
 const dayOfWeek = "日 月 火 水 木 金 土";
-let calcOutputStartPosition = (date) => {
+let calcFirstdayDisplayPosition = (date) => {
   return date.get("d") * 3 + 2;
 };
 
-console.log(yearAndMonth.padStart(13));
+console.log(yearAndMonth.padStart(YearMonthDisplayPosition));
 console.log(dayOfWeek);
 
 for (let date = firstDate; date < lastDate; date = date.add(1, "day")) {
   if (date === firstDate) {
     process.stdout.write(
-      String(date.date()).padStart(calcOutputStartPosition(date)) + " "
+      String(date.date()).padStart(calcFirstdayDisplayPosition(date)) + " "
     );
   } else {
     const numberOfDigit = 2;
