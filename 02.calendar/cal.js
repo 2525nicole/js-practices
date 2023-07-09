f8ad15dd1cceabe550ba091bea5f5a3604b1c911
nicole2525 <nicole2525nomail@gmail.com>
2023-07-09 15:38:13 +0900

    カレンダー表示処理を修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -4,25 +4,29 @@ import ja from "dayjs/locale/ja.js";
 dayjs.locale(ja);
 
 const argv = minimist(process.argv.slice(2));
-const baseDate = dayjs()
-  .year(argv.y == null ? dayjs().get("year") : argv.y)
-  .month(argv.m == null ? dayjs().get("month") : argv.m - 1);
+let baseDate = dayjs();
+if (argv.y != null) baseDate = baseDate.set("year", argv.y);
+if (argv.m != null) baseDate = baseDate.set("month", argv.m - 1);
 const firstDate = baseDate.startOf("month");
 const lastDate = baseDate.endOf("month");
 const yearAndMonth = baseDate.format("MMM YYYY");
 const dayOfWeek = "日 月 火 水 木 金 土";
+let calcOutputStartPosition = (date) => {
+  return date.get("d") * 3 + 2;
+};
 
 console.log(yearAndMonth.padStart(13));
 console.log(dayOfWeek);
 
 for (let date = firstDate; date < lastDate; date = date.add(1, "day")) {
-  let blankNumber;
   if (date === firstDate) {
-    blankNumber = date.get("d") * 3 + 2;
+    process.stdout.write(
+      String(date.date()).padStart(calcOutputStartPosition(date)) + " "
+    );
   } else {
-    blankNumber = 2;
+    const numberOfDigit = 2;
+    process.stdout.write(String(date.date()).padStart(numberOfDigit) + " ");
   }
-  process.stdout.write(String(date.date()).padStart(blankNumber) + " ");
   if (date.get("d") === 6) {
     process.stdout.write("\n");
   }
