1742e8b80aede2220c1216933c19e7808652756e
nicole2525 <nicole2525nomail@gmail.com>
2023-07-13 19:55:43 +0900

    曜日取得の記述を統一

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -26,7 +26,7 @@ for (
   date = date.add(1, "day")
 ) {
   process.stdout.write(String(date.date()).padStart(2) + " ");
-  if (date.get("d") === 6 || date.isSame(lastDate, "day")) {
+  if (date.day() === 6 || date.isSame(lastDate, "day")) {
     process.stdout.write("\n");
   }
 }
